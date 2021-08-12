1b65967ddd70484b3655bdd1c7cb5c0eeeefccaa
Zachary Rener <me@zacharyrener.com>
2021-08-11 19:37:15 -0500

    Font: Added Montserrat

---
+++ components/Layout/header.js
@@ -0,0 +1,10 @@
+import Head from 'next/head'
+
+export default function Header() {
+    return(
+        <Head>
+            <link rel="preconnect" href="https://fonts.googleapis.com" />
+            <link href="https://fonts.googleapis.com/css2?family=Montserrat" rel="stylesheet" />
+        </Head>
+    )
+}

2aa51d43140cbb835b195ffb7a46252aed07aa73
Zachary Rener <me@zacharyrener.com>
2024-10-16 20:05:00 -0500

    add and enable maintenance mode

--- pages/_app.js
+++ pages/_app.js
@@ -6,8 +6,7 @@ import NProgress from "nprogress";
 import "nprogress/nprogress.css";
 import { useEffect } from "react";
 
-// Configure NProgress
-// NProgress.configure({ showSpinner: false, speed: 400, minimum: 0.3 });
+const isMaintenanceMode = true;
 
 Router.events.on("routeChangeStart", () => {
     NProgress.start();
@@ -23,6 +22,50 @@ Router.events.on("routeChangeError", () => {
 
 function MyApp({ Component, pageProps }) {
     useEffect(() => {}, []);
+    if (isMaintenanceMode) {
+        return (
+            <div className="maintenance">
+                <p>
+                    Briefly unavailable for scheduled maintenance. Check back an
+                    hour or so.
+                </p>
+                <ul>
+                    <li>
+                        <a
+                            target="_blank"
+                            href="https://docs.google.com/document/d/1TIZ4wakhXfRBrbQAm_x_dapY2KwqyK7dH9bMZ-bk4Nw/edit?usp=sharing"
+                        >
+                            Resume
+                        </a>
+                    </li>
+                    <li>
+                        <a
+                            target="_blank"
+                            href="https://docs.google.com/document/d/1F7Xmob4Jh8ImSJ9EataWQ7xxaf8bIx4cs6jWiF7IfBw/edit#heading=h.hnwatklyy346"
+                        >
+                            Code Samples
+                        </a>
+                    </li>
+                    <li>
+                        <a
+                            target="_blank"
+                            href="https://github.com/ZacharyRener"
+                        >
+                            GitHub
+                        </a>
+                    </li>
+                    <li>
+                        <a
+                            target="_blank"
+                            href="https://us18.list-manage.com/contact-form?u=c3aee447c73a5506280ee1d82&form_id=037fdd1db95dae27e6e70b47841aa70b"
+                        >
+                            Contact
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        );
+    }
     return <Component {...pageProps} />;
 }
 
